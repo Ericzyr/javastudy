9d754a345ecb80d67d906d386ac0c428546db2f6
ext-yuren.zhang <ext-yuren.zhang@le.com>
2018-04-26 14:51:52 +0800

    javapc

---
+++ day_2.java
@@ -0,0 +1,15 @@
+public class day_2 {
+	public static void main (String[] args){
+		System.out.println("hello wold");
+		
+		int a =2;
+		int b =3;
+		if (a == 2){
+			System.out.println("a");
+		}
+		else{
+			System.out.println("b");
+		}
+	} 
+	
+}
---
+++ day_3.java
@@ -0,0 +1,11 @@
+
+public class day_3 {
+
+	public static String wait(String args){
+        return args;
+    }
+	
+	public static void main(String[] args) {
+		System.out.print("");
+	}
+}
