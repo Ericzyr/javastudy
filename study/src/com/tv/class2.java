b9bcbf2b067693fab6c719a4528388957cc1bf60
ext-yuren.zhang <ext-yuren.zhang@le.com>
2018-05-04 15:20:25 +0800

    pc

--- study/src/com/tv/class2.java
+++ study/src/com/tv/class2.java
@@ -12,7 +12,22 @@ public class class2 {
 		int age  = input.getAge(18);
 		System.out.print("\n"+"有参数有返回的类型："+age);
 		
-		
+/*	权限								没写
+ *         public   protected     default    private
+ * 同一个类  OK          OK          OK         OK
+ * 同一个包	OK			OK			OK
+ * 子类		OK			OK
+ * 不同包中  OK
+ * 
+ * 类只能被public 和default 访问权限修饰符修饰
+ * 当一个记事本中定义了多个类，只能出出一个public的类
+ * 
+ * 类中的成员可以被以上四种访问权限修饰符修饰
+ * 
+ * 
+ * 
+ * */		
+	
 		
 	}
 }
